9f983726e7d1b7d57e7a58ecca3efe1d7606df96
Nick <pastooo@gmail.com>
2023-01-03 21:37:47 -0800

    Set up AS2 "ExternalInterface" demo that uses Javascript game pad API for use with Ruffle

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # RufflePad
-Interface between Javascript game pad methods and ActionScript 2 for use with the Ruffle Flash emulator
+Interface between Javascript game pad methods and ActionScript 2 "ExternalInterface" for use with the Ruffle Flash emulator.
+Uses the well-documented Javascript Game Pad API: https://developer.mozilla.org/en-US/docs/Web/API/Gamepad_API/Using_the_Gamepad_API
+Gamepad layout follows the standard found here: https://w3c.github.io/gamepad/#remapping
+To view and edit the AS2 code within output.swf you will need a copy of the Flash IDE (I use cs5.5)
+
+Consider uploading your resulting project file(s) to Newgrounds for the Flash Forward Jam!
+https://www.newgrounds.com/bbs/topic/1517301
+
+Contact project organizer, PestoForce (aka: Nick Pasto) here:
+https://pestoforce.com/contact.php
+
+Note: To test locally, you must serve the files over a local web server (Ruffle limitation).
+We recommend: https://simplewebserver.org/download.html
---
+++ gamepad.js
@@ -0,0 +1,78 @@
+let flashObj = null;
+let jsLoggingEnabled = false;
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    myInit();
+});
+
+window.addEventListener("gamepadconnected", (e) => {
+    jsLog("Gamepad connected at index %d: %s. %d buttons, %d axes.", e.gamepad.index, e.gamepad.id, e.gamepad.buttons.length, e.gamepad.axes.length);
+
+    if (!!flashObj && !!flashObj.handlePadEvent) {
+        flashObj.handlePadEvent("gamepadconnected", e.gamepad.index);
+    }
+});
+
+window.addEventListener("gamepaddisconnected", (e) => {
+    jsLog("Gamepad disconnected from index %d: %s", e.gamepad.index, e.gamepad.id);
+
+    if (!!flashObj && flashObj.handlePadEvent) {
+        flashObj.handlePadEvent("gamepaddisconnected", e.gamepad.index);
+    }
+});
+
+function myInit() {
+    // once the DOM is loaded, store a ref to the swf object
+    jsLog("DOM Initialized!");
+    flashObj = document.getElementById("output");
+}
+
+/*
+Called from within the .swf file via ExternalInterface to request the state of a specified gamepad by index.
+Function finishes by triggering receievPadState within the .swf file and provides:
+
+index: number = the index of the requested gamepad
+
+buttons: string[] = an array of gamepad buttons with either "true" or "false" for each gamepad button index
+based on whether the button is currently pressed
+
+axes: number[] = an array of axis values ranging from -1 to 1 for each axis on the controller indexed
+by the axis' location on the gamepad
+*/
+function requestPadState(index) {
+    // We must re-query gamepads afresh to get current state (Chrome limitation)
+    const gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+
+    if (!gamepads[index]) {
+        return;
+    }
+
+    for (const gamepad of gamepads) {
+        if (+gamepad.index == +index) {
+            const buttons = [];
+            const axes = [];
+
+            for (var i=0; i<gamepad.buttons.length; i++) {
+              var val = gamepad.buttons[i];
+              buttons[i] = val.pressed;
+            }
+
+            for (var i=0; i<gamepad.axes.length; i++) {
+              var val = gamepad.axes[i];
+              axes[i] = val;
+            }
+
+            flashObj.receivePadState(index, buttons, axes);
+        }
+    }
+}
+
+/*
+Only log to js console if jsLogging is enabled
+*/
+function jsLog(msg) {
+    if (jsLoggingEnabled) {
+        console.log(msg);
+    }
+}
